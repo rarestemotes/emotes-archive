a48cb34e2527f34a32f8eabc091ec52efe3d0331
rarestemotes <traczjanusz47@gmail.com>
2025-05-20 12:51:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const buttonArray = document.querySelectorAll("button");
+const delay = ms => new Promise(res => setTimeout(res, ms));
 buttonArray.forEach(button => {
     button.addEventListener('click', () => {
         navigator.clipboard.writeText(button.id);
 	});
-});
-const delay = ms => new Promise(res => setTimeout(res, ms));
-buttonArray.forEach(button => {
     button.addEventListener('click', async () => {
 		button.innerHTML = "Copied!";
 		await delay(4000);
